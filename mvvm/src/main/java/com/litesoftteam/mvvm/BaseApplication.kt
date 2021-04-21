cad56eb4d395a29fb043804bfab12ba25a85753c
javavirys <javavirys@gmail.com>
2021-04-21 17:16:23 +0300

    Deploy mvvm module to jetpack.io

--- mvvm/src/main/java/com/litesoftteam/mvvm/BaseApplication.kt
+++ mvvm/src/main/java/com/litesoftteam/mvvm/BaseApplication.kt
@@ -4,6 +4,11 @@ import android.app.Application
 import androidx.annotation.CallSuper
 import com.litesoftteam.mvvm.util.logger.LoggerFactory
 import com.litesoftteam.mvvm.util.logger.TimberLogger
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+import org.koin.core.module.Module
 
 abstract class BaseApplication : Application() {
 
@@ -11,7 +16,14 @@ abstract class BaseApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         LoggerFactory.initialize(getLogger())
+        startKoin {
+            androidLogger(Level.DEBUG)
+            androidContext(this@BaseApplication)
+            modules(getKoinModules())
+        }
     }
 
     protected open fun getLogger() = TimberLogger()
+
+    protected open fun getKoinModules() = emptyList<Module>()
 }
