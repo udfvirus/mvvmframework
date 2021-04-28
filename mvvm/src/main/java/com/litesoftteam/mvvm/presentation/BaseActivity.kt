048f815448fe4b9d3e98a3ec56f338b6b4d6deca
javavirys <javavirys@gmail.com>
2021-04-29 02:09:06 +0300

    Add apache licence rules

--- mvvm/src/main/java/com/litesoftteam/mvvm/presentation/BaseActivity.kt
+++ mvvm/src/main/java/com/litesoftteam/mvvm/presentation/BaseActivity.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2021 Vitaliy Sychov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.litesoftteam.mvvm.presentation
 
 import android.os.Bundle
@@ -48,13 +63,11 @@ abstract class BaseActivity : AppCompatActivity {
     protected fun <T> observeEvent(liveData: LiveData<Event<T>>,
                                    result: EventWithSuccessAndError<T>) {
         liveData.observe(this) {
-            when (it.status) {
-                Event.Status.SUCCESS -> {
-                    result.success(it.data!!)
-                }
-                Event.Status.ERROR -> {
-                    result.error(it.throwable!!)
-                }
+            if (it.status == Event.Status.SUCCESS) {
+                result.success(it.data!!)
+            }
+            else if (it.status == Event.Status.ERROR) {
+                result.error(it.throwable!!)
             }
         }
     }
